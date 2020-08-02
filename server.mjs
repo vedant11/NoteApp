364783ac9b81d6e8c3b68b9d4a4aff5f1a8fea97
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2020-08-02 18:03:16 +0530

    shifted from knex to native pg queries

--- server.mjs
+++ server.mjs
@@ -1,65 +1,68 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import knex from 'knex';
-
+import pg from 'pg';
 
 // Init processes and configs
 dotenv.config();
 const DBport = process.env.PORT;
 const serverPort = process.env.SERV_PORT;
-const knexConfig = {
-    client: 'pg',
-    connection: {
-        host: process.env.HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
-    },
-    pool: { min: 0, max: 7 }
+const pgconfig = {
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 }
+const pgClient = new pg.Client(pgconfig);
 
 
-// Init apps
-var db = knex(knexConfig);
-let app = express();
+// Init serveExps
+pgClient.connect();
+let serveExp = express();
+
+
 // creating new table if doesn't exist
-db.schema.hasTable('persons').then(exists => {
-    if (exists === true)
-        console.log('exists');
-    else {
-        console.log('doesnt exist');
-        db.schema.createTable('persons', table => {
-            table.increments('id');
-            table.string('username');
-            table.string('text');
-        }).then();
+pgClient.query('SELECT * from persons', (err, res) => {
+    if (err) {
+        pgClient.query('CREATE TABLE persons (username text NOT NULL PRIMARY KEY,text text);'
+        ).then(() => {
+            console.log('added new table');
+        }).catch((err) => {
+            console.log('err adding table', err);
+        })
     }
+    else
+        console.log('table exists');
+    // pgClient.end();
 });
 
 
 // middleware
-app.use(bodyParser.json());
+serveExp.use(bodyParser.json());
+
 
 //server handlers
-app.get('/users', (req, res) => {
+serveExp.get('/users', (req, res) => {
     const msg = 'your notes are safe';
     res.send(JSON.stringify(msg));
 });
 
-app.post('/register', (req, res) => {
-    const { firstname, lastname, city } = req.body;
-    db('persons').insert({
-        lastname: firstname,
-        firstname: lastname,
-        address: 'bs',
-        city: city
-    }).then(console.log);
-    res.end('success');
+const Qtext = 'INSERT INTO persons(username, text) VALUES($1, $2)';
+serveExp.post('/register', (req, res) => {
+    const { username, text } = req.body;
+    pgClient.query(Qtext, [username, text]
+    ).then(() => {
+        console.log('added to the DB: ', username, text);
+        res.json({ status: 'added', success: 'ok' });
+    }).catch((err) => {
+        console.log('error posting', err);
+        res.json({ status: 'not addedd', success: 'no' });
+    });
 });
 
+
 //Port listening
-app.listen(serverPort, () => {
+serveExp.listen(serverPort, () => {
     console.log(`NoteApp server is running at port http://localhost:${serverPort}`);
     console.log(`NoteApp DB is running at port http://localhost:${DBport}`);
 });
